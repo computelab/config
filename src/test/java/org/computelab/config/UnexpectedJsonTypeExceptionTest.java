f2238bc6467b9f1f24c7dad66cc06c2beec0ed8b
Eric Wu <ericwuyi@gmail.com>
2016-12-16 01:50:43 -0800

    Remove tests on instanceof

--- src/test/java/org/computelab/config/UnexpectedJsonTypeExceptionTest.java
+++ src/test/java/org/computelab/config/UnexpectedJsonTypeExceptionTest.java
@@ -1,17 +1,10 @@
 package org.computelab.config;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class UnexpectedJsonTypeExceptionTest {
-
-    @Test
-    public void isInstanceOfRuntimeException() {
-        UnexpectedJsonTypeException ex = new UnexpectedJsonTypeException("json");
-        assertTrue(ex instanceof RuntimeException);
-    }
 
     @Test
     public void testJson() {
