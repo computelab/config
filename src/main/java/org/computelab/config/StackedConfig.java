7a7bac6fd0659cb9fd5617eb8d9a860fdfa06eb3
neurite <neurite@users.noreply.github.com>
2016-12-28 23:38:34 -0800

    Test StackedConfig

--- src/main/java/org/computelab/config/StackedConfig.java
+++ src/main/java/org/computelab/config/StackedConfig.java
@@ -5,8 +5,14 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 final class StackedConfig extends AbstractConfig {
+
+    private final Logger logger = LoggerFactory.getLogger(StackedConfig.class);
 
     private final List<Config> configs;
 
@@ -15,16 +21,20 @@ final class StackedConfig extends AbstractConfig {
         checkNotNull(configs);
         checkArgument(configs.size() > 0);
         this.configs = Collections.unmodifiableList(configs);
+        logger.info("Stacking configs [" + String.join(", ",
+                this.configs.stream().map(
+                        config -> config.name()
+                ).collect(Collectors.toList())) + "]");
     }
 
     @Override
-    public boolean has(String key) {
+    public boolean has(final String key) {
         final Config config = search(key);
         return config != null;
     }
 
     @Override
-    public String get(String key) {
+    public String get(final String key) {
         final Config config = search(key);
         if (config != null) {
             return config.get(key);
@@ -33,7 +43,7 @@ final class StackedConfig extends AbstractConfig {
     }
 
     @Override
-    public List<String> getAsList(String key) {
+    public List<String> getAsList(final String key) {
         final Config config = search(key);
         if (config != null) {
             return config.getAsList(key);
@@ -44,9 +54,11 @@ final class StackedConfig extends AbstractConfig {
     private Config search(String key) {
         for (final Config config : configs) {
             if (config.has(key)) {
+                logger.info("Using " + config.name() + " for key " + key + ".");
                 return config;
             }
         }
+        logger.warn("Key " + key + " is not defined in any of the configs.");
         return null;
     }
 }
