c6ae95f34344a6adf855f5685cd3acd329d62ba7
neurite <neurite@users.noreply.github.com>
2016-12-30 10:53:29 -0800

    Test ConfigBuilder name

---
+++ src/test/java/org/computelab/config/ConfigBuilderTest.java
@@ -0,0 +1,24 @@
+package org.computelab.config;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ConfigBuilderTest {
+
+    @Test
+    public void testDefaultName() {
+        ConfigBuilder builder = new ConfigBuilder();
+        builder.addJsonConfig("{}");
+        Config config = builder.build();
+        assertEquals(StackedConfig.class.getSimpleName(), config.name());
+    }
+
+    @Test
+    public void testName() {
+        ConfigBuilder builder = new ConfigBuilder();
+        builder.addJsonConfig("{}");
+        Config config = builder.build("config");
+        assertEquals("config", config.name());
+    }
+}
