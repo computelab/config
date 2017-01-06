c1fe7982b52513bf1bb37912dbf418b36ee8a85a
neurite <neurite@users.noreply.github.com>
2017-01-05 23:52:02 -0800

    Add DefaultConfigBuilder

---
+++ src/main/java/org/computelab/config/DefaultConfigBuilder.java
@@ -0,0 +1,103 @@
+package org.computelab.config;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Builds a <code>Config</code> that reads the following sources in order:
+ * <p>
+ * <ol>
+ * <li> System properties which are usually passed on command-line as -D parameters.
+ * <li> Environment variables.
+ * <li> User's home directory, optional.
+ * <li> Resource in the source code, optional.
+ * </ol>
+ */
+public final class DefaultConfigBuilder {
+
+    public static final String DEFAULT_CONFIG_FILE = "app.properties";
+
+    private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+
+    private final Logger logger = LoggerFactory.getLogger(DefaultConfigBuilder.class);
+
+    private final String appName;
+    private final String configFile;
+
+    public DefaultConfigBuilder(final String appName) {
+        this(appName, DEFAULT_CONFIG_FILE);
+    }
+
+    public DefaultConfigBuilder(final String appName, final String configFile) {
+        checkNotNull(appName, "App name must not be null.");
+        checkArgument(!appName.isEmpty(), "App name must not be empty.");
+        checkNotNull(configFile, "Config file name must not be null.");
+        checkArgument(!configFile.isEmpty(), "Config file name must not be empty.");
+        this.appName = appName;
+        this.configFile = configFile;
+    }
+
+    public Config build() {
+        final ConfigBuilder builder = new ConfigBuilder();
+        builder.addSystemPropertyConfig();
+        builder.addSystemEnvConfig();
+        final Properties homeProps = getFromHome();
+        if (homeProps != null) {
+            builder.addPropertiesConfig(homeProps);
+        }
+        final Properties srcProps = getFromSource();
+        if (srcProps != null) {
+            builder.addPropertiesConfig(srcProps);
+        }
+        return builder.build();
+    }
+
+    private Properties getFromHome() {
+        final String userHome = System.getProperty("user.home");
+        final String appHome = "." + appName;
+        final String configFilePath = String.join(File.separator,  userHome, appHome, configFile);
+        try (final FileInputStream inputStream = new FileInputStream(configFilePath);) {
+            return getProperties(inputStream);
+        } catch (final FileNotFoundException ex) {
+            logger.warn("Missing config file " + configFilePath + ". Skipping it...");
+            return null;
+        } catch (final IOException ex) {
+            logger.warn("Error reading config file " + configFile + ". Skipping it...", ex);
+            return null;
+        }
+    }
+
+    private Properties getFromSource() {
+        try (final InputStream inputStream = getClass().getResourceAsStream("/" + configFile)) {
+            if (inputStream == null) {
+                logger.warn("Missing resource " + configFile + ". Skipping it...");
+                return null;
+            }
+            return getProperties(inputStream);
+        } catch (final IOException ex) {
+            logger.warn("Error reading resource " + configFile + ". Skipping it...", ex);
+            return null;
+        } 
+    }
+
+    private Properties getProperties(final InputStream inputStream) throws IOException {
+        try (final InputStreamReader reader = new InputStreamReader(inputStream, DEFAULT_CHARSET)) {
+            final Properties props = new Properties();
+            props.load(reader);
+            return props;
+        }
+    }
+}
---
+++ src/test/java/org/computelab/config/DefaultConfigBuilderTest.java
@@ -0,0 +1,57 @@
+package org.computelab.config;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.junit.Test;
+
+public class DefaultConfigBuilderTest {
+
+    @Test
+    public void testConstructor() throws IllegalAccessException {
+        final String appName = getClass().getSimpleName();
+        final DefaultConfigBuilder builder = new DefaultConfigBuilder(appName);
+        assertEquals(appName, FieldUtils.readField(
+                builder, "appName", true).toString());
+        assertEquals("app.properties", FieldUtils.readField(
+                builder, "configFile", true).toString());
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void constructorAppNameCannotBeNull() {
+        new DefaultConfigBuilder(null, "configFile");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void constructorAppNameCannotBeEmpty() {
+        new DefaultConfigBuilder("", "configFile");
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void constructorConfigFileCannotBeNull() {
+        new DefaultConfigBuilder("app", null);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void constructorConfigFileCannotBeEmpty() {
+        new DefaultConfigBuilder("app", "");
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testBuild() throws IllegalAccessException {
+        final String appName = getClass().getSimpleName();
+        final DefaultConfigBuilder builder = new DefaultConfigBuilder(appName);
+        final Config config = builder.build();
+        assertTrue(config instanceof StackedConfig);
+        final List<Config> configs = (List<Config>)FieldUtils.readField(config, "configs", true);
+        assertNotNull(configs);
+        assertEquals(2, configs.size());
+        assertTrue(configs.get(0) instanceof SystemPropertyConfig);
+        assertTrue(configs.get(1) instanceof SystemEnvConfig);
+    }
+}
